41f02077f684837354cc1b55cb7cd1e832b1d2c9
Alexander Zommer <azoomer2@gmail.com>
2021-09-03 19:29:18 -0400

    Second Push with Code

---
+++ src/main/java/Solution02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String input = scan.nextLine();
+        int length = input.length();
+        System.out.println(input + " has " + length + " characters.");
+
+    }
+}
+
+/* Pseudo Code
+
+    Prompt the user for the input string
+    System.in 'input'
+    length = input.length()
+    System.out to print all at once
+
+
+
+
+
+ */
